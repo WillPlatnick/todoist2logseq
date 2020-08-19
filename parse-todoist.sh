b24cc056f9e6ef8c041387ec238b0192bee25419
William Platnick <wplatnick@carnival.com>
2020-08-18 20:34:35 -0400

    Code

---
+++ parse-todoist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Add any tags you want to auto append
+ADD_TAG="#Today"
+
+# If you want to ignore tasks with a specific time, set this to 1
+IGNORE_TIME=1
+
+todoist sync
+
+while IFS=, read -r id priority date project labels title
+do
+    complete=0
+    if [[ $IGNORE_TIME -eq 1 ]]; then
+      if echo $date | grep 00:00 > /dev/null; then
+        echo " - {{[[TODO]]}} $title $project $ADD_TAG"
+        todoist close $id
+      fi
+    fi
+done < <(/usr/local/bin/todoist --csv list --filter 'today')
+
