cfc5b299ebe977dd413d11600ce8967f151132b4
Will Platnick <will@durin.io>
2021-01-23 13:21:10 -0500

    Spark support

--- parse-todoist.sh
+++ parse-todoist.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+#set -x
 
 # Add any tags you want to auto append
 ADD_TAG=""
 
+# Move emails that Sparks add to today's list of stuff
+PARSE_SPARK=1
+
 # If you want to ignore tasks with a specific time, set this to 1
+# Does not include parsed emails
 IGNORE_TIME=1
 
 /usr/local/bin/todoist sync
+
+if [[ $PARSE_SPARK -eq 1 ]]; then
+  while IFS=, read -r id priority date project labels title
+  do
+    complete=0
+    url=$(/usr/local/bin/todoist show $id | grep ^URL | grep -o 'readdlespark://[^"]*') 
+    newtitle=$(echo $title | sed 's/(Open email in spark)//i' | sed 's/\[//i' | sed 's/\]//i')     
+    echo "{{[[TODO]]}} $newtitle [Open email in spark]($url) $project $ADD_TAG"
+    /usr/local/bin/todoist close $id
+  done < <(/usr/local/bin/todoist --csv list --filter 'today | overdue' | grep 'Open email in Spark')
+fi
+
 
 while IFS=, read -r id priority date project labels title
 do
@@ -17,5 +34,5 @@ do
         /usr/local/bin/todoist close $id
       fi
     fi
-done < <(/usr/local/bin/todoist --csv list --filter 'today' | grep -v 'in Spark')
+done < <(/usr/local/bin/todoist --csv list --filter 'today | overdue')
 
